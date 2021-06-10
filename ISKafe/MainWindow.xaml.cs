df92898540a362523457e257894cfe7d59e5910b
ViperisPRO <Valer4ukpro583@gmail.com>
2021-06-10 19:05:26 +0300

    1234

---
+++ ISKafe/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ISKafe
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private bool _PasswordVisibility = true;
+
+        private bool PasswordVisibility
+        {
+            get
+            {
+                return _PasswordVisibility;
+            }
+            set
+            {
+                _PasswordVisibility = value;
+                PasswordGrid.Visibility = PasswordVisibility ? Visibility.Visible : Visibility.Collapsed;
+                ContentGrid.Visibility = PasswordVisibility ? Visibility.Collapsed : Visibility.Visible;
+                if (PropertyChanged != null)
+                {
+                    // обновляем не каждое поле по отдельности, а все окно
+                    PropertyChanged(this, new PropertyChangedEventArgs("Root"));
+                }
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            PasswordVisibility = true;
+            DataContext = this;
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            // тут нужно конечно навернуть проверку логина и пароля
+            PasswordVisibility = !PasswordVisibility;
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void MenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            var d = new MenuWindow();
+            d.Show();
+        }
+    }
+    }
---
+++ ISKafe/MenuWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ISKafe
+{
+    /// <summary>
+    /// Логика взаимодействия для MenuWindow.xaml
+    /// </summary>
+    public partial class MenuWindow : Window
+    {
+        public MenuWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitMenuWindow_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void SearchTextBox_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void SearchTextBox_TouchEnter(object sender, TouchEventArgs e)
+        {
+
+        }
+    }
+}
